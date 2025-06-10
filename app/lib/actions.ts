c95666cc4fd5495fd205bc57a9fba9a71291fed2
Luana Reginato Bassanesi <lrbassanesi@rd.com.br>
2025-06-10 14:31:22 -0300

    feat: auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
@@ -8,8 +11,12 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({ invalid_type_error: "Please select a customer." }),
-  amount: z.coerce.number() .gt(0, { message: 'Please enter an amount greater than $0.' }),,
-  status: z.enum(["pending", "paid"],{ invalid_type_error: 'Please select an invoice status.',}),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
@@ -24,16 +31,16 @@ export type State = {
 };
 
 export async function createInvoice(prevState: State, formData: FormData) {
-   const validatedFields = CreateInvoice.safeParse({
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
-   if (!validatedFields.success) {
+  if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
+      message: "Missing Fields. Failed to Create Invoice.",
     };
   }
   const { customerId, amount, status } = validatedFields.data;
@@ -79,4 +86,21 @@ export async function deleteInvoice(id: string) {
   revalidatePath("/dashboard/invoices");
 }
 
-
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
